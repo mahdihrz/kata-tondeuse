35f83cba99e49702d1f1c5986f9fa2698c1231a2
mahdihrz <mahdihrz@gmail.com>
2024-01-25 22:08:09 +0100

    refactoring

--- src/main/java/kata/katatondeuse/TondeuseJob.java
+++ src/main/java/kata/katatondeuse/TondeuseJob.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -19,6 +20,8 @@ public class TondeuseJob implements Job {
 
     final private JobRepository jobRepository;
     private static final Logger logger = LoggerFactory.getLogger(TondeuseJob.class);
+    @Autowired
+    private TondeuseService tondeuseService;
 
     public TondeuseJob(JobRepository jobRepository) {
         this.jobRepository = jobRepository;
@@ -35,42 +38,66 @@ public class TondeuseJob implements Job {
         String inputFile = jobParameters.getString("input.file");
         logger.debug("processing tondeuse from file " + inputFile);
 
-        Terrain terrain;
-        List<Tondeuse> tondeuses = new ArrayList<>();
-        TondeuseService tondeuseService = new TondeuseService();
-
-
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
-            String firstLine, tondeuseLine, instructionLine;
-            if ((firstLine = br.readLine()) != null) {
-                String[] terrainCoordinates = firstLine.split(" ");
-                terrain = new Terrain(Integer.parseInt(terrainCoordinates[0]), Integer.parseInt(terrainCoordinates[1]));
-                logger.debug(terrain.toString());
-
-            } else {
-                throw new TondeuseException("no lines in file");
-            }
-
-            while ((tondeuseLine = br.readLine()) != null && (instructionLine = br.readLine()) != null) {
-                String[] tondeuseCoordinates = tondeuseLine.split(" ");
-                Tondeuse tondeuse = new Tondeuse(Integer.parseInt(tondeuseCoordinates[0]), Integer.parseInt(tondeuseCoordinates[1]), tondeuseCoordinates[2].charAt(0), terrain);
-                tondeuses.add(tondeuse);
-                logger.debug("Tondeuse init:" + tondeuse);
-                tondeuseService.processInstructions(tondeuse, instructionLine);
-            }
-
-            StringBuilder sb = new StringBuilder();
-            tondeuses.forEach(s -> sb.append(s).append(" "));
-            logger.info(sb.toString());
-
-        } catch (IOException | TondeuseException e) {
-            logger.error(e.getMessage());
+        if (inputFile == null || inputFile.isEmpty()) {
+            errorOccurs(execution, "input file path empty");
+            return;
         }
 
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+
+            var terrain = initTerrain(br);
+            var tondeuses = bougeLesTondeuses(br, terrain);
+            afficheLeResultat(tondeuses);
+
+        } catch (IOException | TondeuseException e) {
+            errorOccurs(execution, e.getMessage());
+            return;
+        }
 
         execution.setStatus(BatchStatus.COMPLETED);
         execution.setExitStatus(ExitStatus.COMPLETED);
         this.jobRepository.update(execution);
 
+    }
+
+    private void errorOccurs(JobExecution execution, String error) {
+        logger.error(error);
+        execution.setStatus(BatchStatus.FAILED);
+        execution.setExitStatus(ExitStatus.FAILED);
+        this.jobRepository.update(execution);
+    }
+
+    private static void afficheLeResultat(List<Tondeuse> tondeuses) {
+        StringBuilder sb = new StringBuilder();
+        tondeuses.forEach(s -> sb.append(s).append(" "));
+        logger.info(sb.toString());
+    }
+
+    private List<Tondeuse> bougeLesTondeuses(BufferedReader br, Terrain terrain) throws IOException {
+        List<Tondeuse> tondeuses = new ArrayList<>();
+        String tondeuseLine;
+        String instructionLine;
+        while ((tondeuseLine = br.readLine()) != null && (instructionLine = br.readLine()) != null) {
+            String[] tondeuseCoordinates = tondeuseLine.split(" ");
+            Tondeuse tondeuse = new Tondeuse(Integer.parseInt(tondeuseCoordinates[0]), Integer.parseInt(tondeuseCoordinates[1]), tondeuseCoordinates[2].charAt(0), terrain);
+            tondeuses.add(tondeuse);
+            logger.debug("Tondeuse init:" + tondeuse);
+            tondeuseService.processInstructions(tondeuse, instructionLine);
+        }
+        return tondeuses;
+    }
+
+    private static Terrain initTerrain(BufferedReader br) throws IOException, TondeuseException {
+        String firstLine;
+        Terrain terrain;
+        if ((firstLine = br.readLine()) != null) {
+            String[] terrainCoordinates = firstLine.split(" ");
+            terrain = new Terrain(Integer.parseInt(terrainCoordinates[0]), Integer.parseInt(terrainCoordinates[1]));
+            logger.debug(terrain.toString());
+
+        } else {
+            throw new TondeuseException("no lines in file");
+        }
+        return terrain;
     }
 }
--- src/main/java/kata/katatondeuse/service/TondeuseService.java
+++ src/main/java/kata/katatondeuse/service/TondeuseService.java
@@ -1,7 +1,9 @@
 package kata.katatondeuse.service;
 
 import kata.katatondeuse.model.Tondeuse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TondeuseService {
     public void processInstructions(Tondeuse tondeuse, String instructions) {
         for (char instruction : instructions.toCharArray()) {
