c349a904305bac0e3d56a319eb09f598d35564af
mahdihrz <mahdihrz@gmail.com>
2024-01-25 18:55:25 +0100

    init with spring project

---
+++ src/main/java/kata/katatondeuse/KataTondeuseApplication.java
@@ -0,0 +1,13 @@
+package kata.katatondeuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataTondeuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataTondeuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kata/katatondeuse/KataTondeuseApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.katatondeuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataTondeuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
